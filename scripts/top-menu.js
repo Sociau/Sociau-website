c028e1a1d9a21ec3fc97a9ee6382226b77a00145
José Lucas <joselucas.freitas@webjump.com.br>
2024-09-05 13:26:23 -0300

    fix: botão voltar

--- scripts/top-menu.js
+++ scripts/top-menu.js
@@ -1,8 +1,16 @@
 document.getElementById('sociau_logo').addEventListener('click', function () {
+    
     if (window.innerWidth < 768) {
         var ul = document.getElementById('menu-list');
         ul.style.display = ul.style.display === 'flex' ? 'none' : 'flex';
     } else {
-        window.location.href = 'index.html';
+        folder = (window.location.href).split('/')[3];
+
+        if (folder == 'pages') {
+            window.location.href = '../index.html';
+        }else{
+            window.location.href = 'index.html';
+        }
+        
     }
 });
