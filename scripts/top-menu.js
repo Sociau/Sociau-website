d7409c53244d44613ee52b0faaa28095ea7613af
José Lucas <joselucas.freitas@webjump.com.br>
2024-09-05 13:53:59 -0300

    fix: script atualizado

--- scripts/top-menu.js
+++ scripts/top-menu.js
@@ -1,16 +1,9 @@
 document.getElementById('sociau_logo').addEventListener('click', function () {
-    
     if (window.innerWidth < 768) {
         var ul = document.getElementById('menu-list');
         ul.style.display = ul.style.display === 'flex' ? 'none' : 'flex';
     } else {
-        folder = (window.location.href).split('/')[4];
-
-        if (folder == 'pages') {
-            window.location.href = '../index.html';
-        }else{
-            window.location.href = 'index.html';
-        }
         
+        window.location.href = 'index.html';
     }
 });
