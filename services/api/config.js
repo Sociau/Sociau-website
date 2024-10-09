3b9b1378fcfeafc7997fb990156daa753fc4166e
José Lucas <joselucas.freitas@webjump.com.br>
2024-10-09 00:18:49 -0300

    fix: url api

--- services/api/config.js
+++ services/api/config.js
@@ -1,6 +1,6 @@
 const config = {
-  //BASE_URL: 'http://127.0.0.1:8888/',
-  BASE_URL:'http://3.141.97.109:8000/'
+  BASE_URL: 'http://127.0.0.1:8888/',
+  //BASE_URL:'http://3.141.97.109:8000/'
 
 }
 
