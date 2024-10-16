a06df363fabcf3f00b2c8746e3752d5aac06b6db
José Lucas <josluc2016@gmail.com>
2024-10-16 16:20:02 -0300

    fix: novo cadastro

--- services/api/index.js
+++ services/api/index.js
@@ -1,7 +1,7 @@
 import config from './config.js'
 
 const login = async (nickname, password) => {
-  try{
+  try {
     const response = await fetch(`${config.BASE_URL}login`, {
       method: 'POST',
       headers: {
@@ -12,31 +12,51 @@ const login = async (nickname, password) => {
         password,
       }),
     })
-  
+
     const data = await response.json()
     if (data.token) {
       localStorage.setItem('token', data.token)
       localStorage.setItem('user', JSON.stringify(data.user))
     }
     return data
-  }catch(error){
+  } catch (error) {
     console.log(error)
   }
 }
 
 const createAccount = async (person) => {
-  try{
+  try {
+
+    const formData = new FormData();
+
+    formData.append('name', person.name);
+    formData.append('nickname', person.nickname);
+    formData.append('email', person.email);
+    formData.append('main_whatsapp', person.main_whatsapp);
+    formData.append('second_whatsapp', person.second_whatsapp);
+    formData.append('about_you', person.about_you);
+    formData.append('password', person.password);
+
+    if (person.address) {
+      formData.append('state', person.address.state);
+      formData.append('city', person.address.city);
+      formData.append('street', person.address.street);
+      formData.append('neighborhood', person.address.neighborhood);
+      formData.append('number', person.address.number);
+    }
+
+    if (avatarFile) {
+      formData.append('avatar', avatarFile);
+    }
+
     const response = await fetch(`${config.BASE_URL}create_account`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(person),
+      body: formData,
     })
-  
+
     const data = await response.json()
     return data
-  }catch(error){
+  } catch (error) {
     console.log(error)
   }
 }
